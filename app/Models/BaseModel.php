e499b86ed1e566f4c09a5477e13f0e56d1c50478
root <root@localhost.localdomain>
2023-01-19 13:15:22 +0900

    a

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -24,34 +24,32 @@ trait BaseModel
     private function createMessage(string $name): string
     {
         $className = className($this);
-        $crud = isset(NameConst::NAMES[NameConst::TYPE_SHORT][$name]) ? NameConst::NAMES[NameConst::TYPE_SHORT][$name] : $name;
-
         $backtrace = debug_backtrace();
         $targetBacktrace = isset($backtrace[1]) ? $backtrace[1] : null;
 
-        if (is_null($targetBacktrace) || !isset($targetBacktrace["file"]) || !isset($targetBacktrace["line"])) return $className . " " . $crud . " backtrace: " . json_encode($backtrace);
+        if (is_null($targetBacktrace) || !isset($targetBacktrace["file"]) || !isset($targetBacktrace["line"])) return $className . " " . $name . " backtrace: " . json_encode($backtrace);
 
         $service = explode("/", $targetBacktrace["file"]);
         $serviceClassName = str_replace(".php", "", end($service));
         $line = $targetBacktrace["line"];
 
-        return $className . " " . $crud . " in " . $serviceClassName . ": " . $line;
+        return $className . " " . $name . " in " . $serviceClassName . ": " . $line;
     }
 
     public function safeCreate(): void
     {
-        $this->safeSave($this->createMessage(NameConst::CREATE));
+        $this->safeSave($this->createMessage(strtoupper(NameConst::CREATE)));
     }
 
     public function safeUpdate(): void
     {
-        $this->safeSave($this->createMessage(NameConst::UPDATE));
+        $this->safeSave($this->createMessage(strtoupper(NameConst::UPDATE)));
     }
 
     public function safeDelete(): void
     {
         assert($this instanceof Model);
-        Transaction($this->createMessage(NameConst::DELETE), function () {
+        Transaction($this->createMessage(strtoupper(NameConst::DELETE)), function () {
             $this->delete();
         });
     }
@@ -61,7 +59,7 @@ trait BaseModel
         assert($this instanceof Model);
         if (isset($this->is_valid)) {
             $this->is_valid = $isValid;
-            $this->safeSave($this->createMessage(NameConst::CHANGE));
+            $this->safeSave($this->createMessage(strtoupper(NameConst::CHANGE)));
         }
     }
 }
