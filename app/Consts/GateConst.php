d711143678607abb607771f86bd60bafa192ec97
takeru <take.yama0519@gmail.com>
2023-02-02 19:26:29 +0900

    a

--- app/Consts/GateConst.php
+++ app/Consts/GateConst.php
@@ -11,12 +11,13 @@ class GateConst
     public const USER          = "user";
     public const GUEST         = "guest";
 
-    public const ADMIN_HIGHER  = "admin-higher";
-    public const USER_HIGHER   = "user-higher";
-
     public const SYSTEM_NUMBER = 1;
     public const ADMIN_NUMBER  = 5;
     public const USER_NUMBER   = 10;
+    public const GUEST_NUMBER  = 100;
+
+    public const ADMIN_HIGHER  = "admin-higher";
+    public const USER_HIGHER   = "user-higher";
 
     public const ROLES = [
         self::SYSTEM => self::SYSTEM_NUMBER,
