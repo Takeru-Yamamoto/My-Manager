615955c9209b3dcacbd9f4009cb6102c81e63547
root <root@localhost.localdomain>
2023-02-07 00:38:35 +0900

    a

--- app/Library/FileUtil/StorageFile/ExcelStorageFile.php
+++ app/Library/FileUtil/StorageFile/ExcelStorageFile.php
@@ -7,6 +7,8 @@ use App\Library\FileUtil\StorageFile;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use PhpOffice\PhpSpreadsheet\Writer\Csv;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 final class ExcelStorageFile extends StorageFile
 {
@@ -29,6 +31,17 @@ final class ExcelStorageFile extends StorageFile
 
     public function save(string $uploadDirectory, string $fileName): self
     {
+        if (strpos($fileName, ".xlsx") === false) $fileName .= ".xlsx";
+        $writer = new Xlsx($this->file);
+        $writer->save($uploadDirectory . "/" . $fileName);
+        $this->reset($uploadDirectory, $fileName);
+        return $this;
+    }
+    public function saveAsCSV(string $uploadDirectory, string $fileName): self
+    {
+        if (strpos($fileName, ".csv") === false) $fileName .= ".csv";
+        $writer = new Csv($this->file);
+        $writer->save($uploadDirectory . "/" . $fileName);
         $this->reset($uploadDirectory, $fileName);
         return $this;
     }
@@ -89,7 +102,7 @@ final class ExcelStorageFile extends StorageFile
         return $this->targetSheetPage;
     }
 
-    public function setSheet(int $page = null): self
+    public function selectSheet(int $page = null): self
     {
         if (is_null($page)) $page = $this->targetSheetPage;
         if ($page >= 0 && $page <= $this->sheetCount) $this->targetSheet = $this->file->getSheet($page);
@@ -98,34 +111,84 @@ final class ExcelStorageFile extends StorageFile
     public function pivotPrev(): self
     {
         if ($this->targetSheetPage !== 0) $this->targetSheetPage--;
-        return $this->setSheet();
+        return $this->selectSheet();
     }
     public function pivotNext(): self
     {
         if ($this->targetSheetPage < $this->sheetCount) $this->targetSheetPage++;
-        return $this->setSheet();
+        return $this->selectSheet();
     }
     public function pivotFirst(): self
     {
         $this->targetSheetPage = 0;
-        return $this->setSheet();
+        return $this->selectSheet();
     }
     public function pivotLast(): self
     {
         $this->targetSheetPage = $this->sheetCount;
-        return $this->setSheet();
+        return $this->selectSheet();
+    }
+    public function addSheet(string $sheetName): self
+    {
+        $sheet = new Worksheet($this->file, $sheetName);
+        $this->file->addSheet($sheet);
+        return $this;
+    }
+    public function sheetTitle(): string
+    {
+        return $this->targetSheet->getTitle();
+    }
+    public function setSheetTitle(string $title): self
+    {
+        $this->targetSheet->setTitle($title);
+        return $this;
     }
 
-    public function allCellValue(): array
-    {
-        return $this->targetSheet->rangeToArray($this->targetSheet->calculateWorksheetDimension(), null, true, true, false);
-    }
     public function cellValue(string $cell): mixed
     {
         return $this->targetSheet->getCell($cell)->getValue();
     }
+    public function setCellValue(string $cell, mixed $value): self
+    {
+        $this->targetSheet->setCellValue($cell, $value);
+        return $this;
+    }
+    public function cellValues(string $range = null): array
+    {
+        if (is_null($range)) $range = $this->targetSheet->calculateWorksheetDimension();
+        return $this->targetSheet->rangeToArray($range, null, true, true, false);
+    }
+    public function setCellValues(array $values, string $startCell = null): self
+    {
+        $this->targetSheet->fromArray($values, null, $startCell, false);
+        return $this;
+    }
     public function cellIsNull(string $cell): bool
     {
         return is_null($this->cellValue($cell));
+    }
+    public function mergeCells(string $range): self
+    {
+        $this->targetSheet->mergeCells($range);
+        return $this;
+    }
+
+    public function rowHeight(int $row): float
+    {
+        return $this->targetSheet->getRowDimension($row)->getRowHeight();
+    }
+    public function setRowHeight(int $row, float $height): self
+    {
+        $this->targetSheet->getRowDimension($row)->setRowHeight($height);
+        return $this;
+    }
+    public function columnWidth(int $column): float
+    {
+        return $this->targetSheet->getColumnDimension($column)->getWidth();
+    }
+    public function setColumnWidth(int $column, float $width): self
+    {
+        $this->targetSheet->getColumnDimension($column)->setWidth($width);
+        return $this;
     }
 }
--- app/Library/FileUtil/StorageFile/TextStorageFile.php
+++ app/Library/FileUtil/StorageFile/TextStorageFile.php
@@ -5,12 +5,17 @@ namespace App\Library\FileUtil\StorageFile;
 use App\Library\FileUtil\Exception\CharacterCodeNotFoundException;
 use App\Library\FileUtil\StorageFile;
 
-use PhpOffice\PhpSpreadsheet\Reader\Csv;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use PhpOffice\PhpSpreadsheet\Reader\Csv as CSVReader;
+use PhpOffice\PhpSpreadsheet\Writer\Csv;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 final class TextStorageFile extends StorageFile
 {
-    public Worksheet $file;
+    public Spreadsheet $file;
+
+    private Worksheet $targetSheet;
 
     private string $characterCode;
     private array $data;
@@ -24,21 +29,34 @@ final class TextStorageFile extends StorageFile
 
     public function save(string $uploadDirectory, string $fileName): self
     {
+        if (strpos($fileName, ".csv") === false) $fileName .= ".csv";
+        $writer = new Csv($this->file);
+        $writer->save($uploadDirectory . "/" . $fileName);
+        $this->reset($uploadDirectory, $fileName);
+        return $this;
+    }
+    public function saveAsXLSX(string $uploadDirectory, string $fileName): self
+    {
+        if (strpos($fileName, ".xlsx") === false) $fileName .= ".xlsx";
+        $writer = new Xlsx($this->file);
+        $writer->save($uploadDirectory . "/" . $fileName);
         $this->reset($uploadDirectory, $fileName);
         return $this;
     }
     protected function setChild(): void
     {
-        $csv = new Csv;
+        $csv = new CSVReader;
 
         $this->characterCode = $this->checkCharacterCode();
-        $this->file          = $csv->setInputEncoding($this->characterCode)->load($this->filePath)->getSheet(0);
-        $this->data          = $this->file->rangeToArray($this->file->calculateWorksheetDimension());
+        $this->file          = $csv->setInputEncoding($this->characterCode)->load(str_replace("public", "storage", $this->filePath));
+        $this->targetSheet   = $this->file->getSheet(0);
+        $this->data          = $this->targetSheet->rangeToArray($this->targetSheet->calculateWorksheetDimension());
     }
     protected function childParams(): array
     {
         return [
             "characterCode" => $this->characterCode(),
+            "targetSheet"   => $this->targetSheet(),
             "data"          => $this->data(),
         ];
     }
@@ -47,9 +65,61 @@ final class TextStorageFile extends StorageFile
     {
         return $this->characterCode;
     }
+    public function targetSheet(): Worksheet
+    {
+        return $this->targetSheet;
+    }
     public function data(): array
     {
         return $this->data;
+    }
+
+    public function cellValue(string $cell): mixed
+    {
+        return $this->targetSheet->getCell($cell)->getValue();
+    }
+    public function setCellValue(string $cell, mixed $value): self
+    {
+        $this->targetSheet->setCellValue($cell, $value);
+        return $this;
+    }
+    public function cellValues(string $range = null): array
+    {
+        if (is_null($range)) $range = $this->targetSheet->calculateWorksheetDimension();
+        return $this->targetSheet->rangeToArray($range, null, true, true, false);
+    }
+    public function setCellValues(array $values, string $startCell = null): self
+    {
+        $this->targetSheet->fromArray($values, null, $startCell, false);
+        return $this;
+    }
+    public function cellIsNull(string $cell): bool
+    {
+        return is_null($this->cellValue($cell));
+    }
+    public function mergeCells(string $range): self
+    {
+        $this->targetSheet->mergeCells($range);
+        return $this;
+    }
+
+    public function rowHeight(int $row): float
+    {
+        return $this->targetSheet->getRowDimension($row)->getRowHeight();
+    }
+    public function setRowHeight(int $row, float $height): self
+    {
+        $this->targetSheet->getRowDimension($row)->setRowHeight($height);
+        return $this;
+    }
+    public function columnWidth(int $column): float
+    {
+        return $this->targetSheet->getColumnDimension($column)->getWidth();
+    }
+    public function setColumnWidth(int $column, float $width): self
+    {
+        $this->targetSheet->getColumnDimension($column)->setWidth($width);
+        return $this;
     }
 
     private function checkCharacterCode(): string
